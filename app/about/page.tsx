81b17ef328ac28a63382bb955db8bc86495af227
KhadijaMahanga <khadijamahanga@y7mail.com>
2024-08-05 12:35:20 +0300

    install tailwind css

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: "About Us",
+  };
+  
+  export default function Page() {
+    return <h1>App Router</h1>;
+  }
+  
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,30 @@
+import type { Metadata } from 'next'
+ 
+
+export const metadata: Metadata = {
+  title: {
+    default: "Kike Africa",
+    template: "%s | Kike Africa",
+  },
+  description: "Kike Africa is a pioneering media and technology company at the forefront of civic technology, and serving as a vibrant coding & data hub for young women in Tanzania. We also offer expert consultation services to drive innovation and success.",
+  metadataBase: new URL("https://kikeafrica.co.tz"),
+  openGraph: {
+    title: "Kike Africa",
+    description: "Kike Africa is a pioneering media and technology company at the forefront of civic technology, and serving as a vibrant coding & data hub for young women in Tanzania. We also offer expert consultation services to drive innovation and success.",
+    url: "https://kikeafrica.co.tz",
+    siteName: "Kike Africa",
+    images: [
+      {
+        url: "https://kikeafrica.co.tz/images/image.jpg",
+        type: "image/jpeg"
+      }
+    ],
+    locale: "en_US",
+    type: "website" 
+  }
+}
+
+
 export default function RootLayout({
   children,
 }: {
